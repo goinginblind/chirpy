c15ae095f62122ef8fd0a2743d78ec5e5ce2cdaa
goinginblind <science.hhughes@gmail.com>
2025-06-15 23:36:28 +0300

    Add enviroment variable support

--- main.go
+++ main.go
@@ -1,34 +1,60 @@
 package main
 
 import (
+	"database/sql"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"sync/atomic"
+
+	"github.com/goinginblind/chirpy/internal/database"
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	Queries        *database.Queries
 }
 
 func main() {
-	const filePathRoot = "."
-	const port = "8080"
-
+	// setup logging
 	logFile, err := os.OpenFile("server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("Fail to open the log file: %s", err)
 	}
+	defer logFile.Close()
 
 	log.SetOutput(io.MultiWriter(os.Stdout, logFile))
 
+	// load enviromental variables
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("No .env file found, proceeding with system environment")
+	}
+	dbURL := os.Getenv("DB_URL")
+	filepathRoot := os.Getenv("FILEPATH_ROOT")
+	port := os.Getenv("PORT")
+
+	// connect to db
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening db: %s", err)
+	}
+	dbQueries := database.New(db)
+	log.Println("Connected to database.")
+
+	// setup config
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		Queries:        dbQueries,
 	}
 
+	// setup multiplexer and handles
 	mux := http.NewServeMux()
-	fsHandler := http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filePathRoot))))
+	fsHandler := http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot))))
 	mux.Handle("/app/", fsHandler)
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
@@ -36,11 +62,12 @@ func main() {
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("POST /api/validate_chirp", handlerValidate)
 
+	// setup server
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
